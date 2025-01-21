74ea684f16bc76b2cb8705b0d6ed94e798fbd61f
alejandro <alegalvezlo2@gmail.com>
2025-01-21 23:12:52 +0100

    Nested

---
+++ Loops/Nested.java
@@ -0,0 +1,17 @@
+package Loops;
+
+public class Nested {
+    public static void main(String [] args){
+        int count = 0;
+        for(int i=1; i<=5;i++){
+            for(int j=1;j<=5;j++){
+                count++;
+                //System.out.print("("+i+","+j+")");
+                //System.out.print(i+j+",");
+                System.out.format("%02d ",count);
+            }
+            System.out.println("");
+        }
+    }
+    
+}
