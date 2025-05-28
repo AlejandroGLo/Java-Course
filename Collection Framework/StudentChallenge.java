f2c609c140e418804ad1b27b69d07a19d6dbddbe
alejandro <alegalvezlo2@gmail.com>
2025-05-28 23:23:16 +0200

    schallenge

---
+++ Collection Framework/StudentChallenge.java
@@ -0,0 +1,106 @@
+import java.io.*;
+import java.util.*;
+
+class Account implements Serializable{
+    String accno;
+    String name;
+    double balance;
+
+    Account(){}
+
+    Account(String a, String n, double b){
+        accno=a;
+        name=n;
+        balance=b;
+    }
+
+    public String toString(){
+        return "Account No:"+accno+"\nName: "+name+"\nBalance: "+balance+"\n";
+    }
+}
+
+public class StudentChallenge {
+    public static void main(String args[]) throws Exception{
+        Scanner sc = new Scanner(System.in);
+
+        Account acc=null;
+        HashMap<String, Account> hm = new HashMap<>();
+
+        try{
+            FileInputStream fis = new FileInputStream("Accounts.txt");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+
+            int count = ois.readInt();
+            for(int i=0; i<count;i++){
+                acc = (Account)ois.readObject();
+                System.out.println(acc);
+                hm.put(acc.accno, acc);
+            }
+
+            fis.close();
+            ois.close();
+        } catch(Exception e){}
+
+        FileOutputStream fos = new FileOutputStream("Accounts.txt");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+        System.out.println("Menu");
+
+        int choice;
+        String accno,name;
+        double balance;
+
+        do{
+            System.out.println("1. Create Account");
+            System.out.println("2. Delete Account");
+            System.out.println("3. View Account");
+            System.out.println("4. View All Accounts");
+            System.out.println("5. Save Accounts");
+            System.out.println("6. Exit");
+            System.out.println("Enter your choice");
+            choice = sc.nextInt();
+            sc.skip("");
+
+            switch (choice) {
+            case 1:
+                System.out.println("Enter Accno, Name, Balance:");
+                accno = sc.next();
+                name = sc.next();
+                balance = sc.nextDouble();
+                acc = new Account(accno, name, balance);
+                hm.put(accno, acc);
+                break;
+            case 2:
+                System.out.print("Enter Account No to delete: ");
+                accno = sc.next();
+                hm.remove(accno);
+                break;
+            case 3:
+                System.out.print("Enter Account No to view: ");
+                accno = sc.next();
+                acc = hm.get(accno);
+                System.out.println(acc != null ? acc : "Account not found.");
+                break;
+            case 4:
+                for (Account a : hm.values()) {
+                    System.out.println(a);
+                }
+                break;
+            case 5:
+                oos.writeInt(hm.size());
+                for (Account a : hm.values()) {
+                    oos.writeObject(a);
+                }
+                oos.flush();
+                System.out.println("Accounts saved.");
+                break;
+        }
+
+    } while (choice != 6);
+
+    oos.close();
+    fos.close();
+    sc.close();
+}
+}
+
